59e72fc4b5f52239f5bf9d9e0ef8b7f78e81435f
Ken O'Neill <kenneth.oneill@gmail.com>
2019-07-22 09:48:02 +0100

    Added sample files for CountVowels

---
+++ countvowels/CountVowels.py
@@ -0,0 +1,5 @@
+class CountVowels:
+    def count(self, word):
+        return {}
+
+
---
+++ countvowels/CountVowelsTest.py
@@ -0,0 +1,12 @@
+import unittest
+from CountVowels import CountVowels
+
+class CountVowelsTest(unittest.TestCase):
+    def test_something(self):
+        expected = {"a":0, "e":0, "i":0, "o":0, "u":0}
+        obj_under_test = CountVowels()
+        self.assertEqual(expected, obj_under_test.count("Rhythm"))
+
+
+if __name__ == '__main__':
+    unittest.main()
